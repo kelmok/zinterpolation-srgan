aaf402ba36561d06ed44a1c67deeb674c613eb12
Kelvin Mok <kelmok@users.noreply.github.com>
2018-12-07 17:01:48 -0500

    Update and rename srgan2d to srgan2d.py

---
+++ srgan2d.py
@@ -0,0 +1,208 @@
+class SRGAN():
+    def __init__(self):
+        # Input shape
+        self.channels = 3
+        self.lr_height = 256                 # Low resolution height
+        self.lr_width = 256                 # Low resolution width
+        self.lr_shape = (self.lr_height, self.lr_width, self.channels)
+        self.hr_height = self.lr_height   # High resolution height
+        self.hr_width = self.lr_width     # High resolution width
+        self.hr_shape = (self.hr_height, self.hr_width, self.channels)
+        
+        optimizer = Adam(0.0002, 0.5)
+        self.image_seq = 1
+        self.n_residual_blocks = 15
+        self.vgg = self.build_vgg()
+        self.vgg.trainable = False
+        self.vgg.compile(loss='mse',
+            optimizer=optimizer,
+            metrics=['accuracy'])
+
+        # Configure data loader
+        self.full_batch_images=[];
+        self.full_batch_images_lr= [];
+        self.trial_name = 'TrialName'
+        self.dataset_name = 'DataIso'
+        self.dataset_name_lr = 'DataAniso'
+        self.dataset_name_real = 'DataValidation'
+
+        self.data_loader = DataLoader(dataset_name=self.dataset_name,dataset_name_lr=self.dataset_name_lr,dataset_name_real=self.dataset_name_real,
+                                      img_res=(self.hr_height, self.hr_width))
+
+        # Calculate output shape of D (PatchGAN)
+        patch = int(self.hr_height / 2**4)
+        self.disc_patch = (patch, patch, 1)
+
+        # Number of filters in the first layer of G and D
+        self.gf = 64
+        self.df = 64
+
+        # Build and compile the discriminator
+        self.discriminator = self.build_discriminator()
+        self.discriminator.compile(loss='mse',
+            optimizer=optimizer,
+            metrics=['accuracy'])
+
+        # Build the generator
+        self.generator = self.build_generator()
+
+        # High res. and low res. images
+        img_hr = Input(shape=self.hr_shape)
+        img_lr = Input(shape=self.lr_shape)
+
+        # Generate high res. version from low res.
+        fake_hr = self.generator(img_lr)
+
+        # Extract image features of the generated img
+        fake_features = self.vgg(fake_hr)
+
+        # For the combined model we will only train the generator
+        self.discriminator.trainable = False
+
+        # Discriminator determines validity of generated high res. images
+        validity = self.discriminator(fake_hr)
+
+        self.combined = Model([img_lr, img_hr], [validity, fake_features])
+        self.combined.compile(loss=['binary_crossentropy', 'mse'],
+                              loss_weights=[1e-3, 1],
+                              optimizer=optimizer)
+
+    def build_vgg(self):
+        """
+        Builds a pre-trained VGG19 model that outputs image features extracted at the
+        third block of the model
+        """
+        vgg = VGG19(weights="imagenet")
+        vgg.outputs = [vgg.layers[9].output]
+
+        img = Input(shape=self.hr_shape)
+
+        # Extract image features
+        img_features = vgg(img)
+
+        return Model(img, img_features)
+
+    def build_generator(self):
+
+        def residual_block(layer_input, filters):
+            """Residual block described in paper"""
+            d = Conv2D(filters, kernel_size=3, strides=1, padding='same')(layer_input)
+            d = Activation('relu')(d)
+            d = BatchNormalization(momentum=0.8)(d)
+            d = Conv2D(filters, kernel_size=3, strides=1, padding='same')(d)
+            d = BatchNormalization(momentum=0.8)(d)
+            d = Add()([d, layer_input])
+            return d
+
+        def deconv2d(layer_input):
+            """Layers used during upsampling"""
+            u = UpSampling2D(size=1)(layer_input)
+            u = Conv2D(256, kernel_size=3, strides=1, padding='same')(u)
+            u = Activation('relu')(u)
+            return u
+
+        # Low resolution image input
+        img_lr = Input(shape=self.lr_shape)
+
+        # Pre-residual block
+        c1 = Conv2D(64, kernel_size=9, strides=1, padding='same')(img_lr)
+        c1 = Activation('relu')(c1)
+
+       # Propogate through residual blocks
+        r = residual_block(c1,self.gf)
+        for _ in range(self.n_residual_blocks - 1):
+            r = residual_block(r, self.gf)
+
+        # Post-residual block
+        c2 = Conv2D(64, kernel_size=3, strides=1, padding='same')(r)
+        c2 = BatchNormalization(momentum=0.8)(c2)
+        c2 = Add()([c2, c1])
+
+        # Upsampling
+        u1 = deconv2d(c2)
+        u2 = deconv2d(u1)
+
+        # Generate high resolution output
+        gen_hr = Conv2D(self.channels, kernel_size=9, strides=1, padding='same', activation='tanh')(u2)
+        return Model(img_lr, gen_hr)
+
+    def build_discriminator(self):
+
+        def d_block(layer_input, filters, strides=1, bn=True):
+            """Discriminator layer"""
+            d = Conv2D(filters, kernel_size=3, strides=strides, padding='same')(layer_input)
+            d = LeakyReLU(alpha=0.2)(d)
+            if bn:
+                d = BatchNormalization(momentum=0.8)(d)
+            return d
+
+        # Input img
+        d0 = Input(shape=self.hr_shape)
+
+        d1 = d_block(d0, self.df, bn=False)
+        d2 = d_block(d1, self.df, strides=2)
+        d3 = d_block(d2, self.df*2)
+        d4 = d_block(d3, self.df*2, strides=2)
+        d5 = d_block(d4, self.df*4)
+        d6 = d_block(d5, self.df*4, strides=2)
+        d7 = d_block(d6, self.df*8)
+        d8 = d_block(d7, self.df*8, strides=2)
+
+        d9 = Dense(self.df*16)(d8)
+        d10 = LeakyReLU(alpha=0.2)(d9)
+        validity = Dense(1, activation='sigmoid')(d10)
+
+        return Model(d0, validity)
+
+    def train(self, epochs, batch_size=1, sample_interval=50):
+        #K.set_session(sess)
+        start_time = datetime.datetime.now()
+
+        for epoch in range(epochs):
+            imgs_hr, imgs_lr , img_name = self.data_loader.load_data(batch_size)
+            fake_hr = self.generator.predict(imgs_lr)
+            valid = np.ones((batch_size,) + self.disc_patch)
+            fake = np.zeros((batch_size,) + self.disc_patch)
+
+            # Train the discriminators (original images = real / generated = Fake)
+            d_loss_real = self.discriminator.train_on_batch(imgs_hr, valid)
+            d_loss_fake = self.discriminator.train_on_batch(fake_hr, fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+            # ------------------
+            #  Train Generator
+            # ------------------
+
+            # Sample images and their conditioning counterparts
+            imgs_hr, imgs_lr , img_name = self.data_loader.load_data(batch_size)
+
+            # The generators want the discriminators to label the generated images as real
+            valid = np.ones((batch_size,) + self.disc_patch)
+
+            # Extract ground truth image features using pre-trained VGG19 model
+            image_features = self.vgg.predict(imgs_hr)
+
+            # Train the generators
+            g_loss = self.combined.train_on_batch([imgs_lr, imgs_hr], [valid, image_features])
+
+            elapsed_time = datetime.datetime.now() - start_time
+            # Plot the progress
+            print ("%d time: %s" % (epoch, elapsed_time))
+
+            # If at save interval => save generated image samples
+            if epoch % sample_interval == 0:
+                self.sample_images(epoch)
+                model_json = self.generator.to_json()
+                with open(("model_%s_epoch_%d.json" % (self.trial_name, epoch)), "w") as json_file:
+                    json_file.write(model_json)
+                # serialize weights to HDF5
+                self.generator.save_weights("model_%s_epoch_%d.h5" % (self.trial_name, epoch), "w")
+                print("Saved model to disk")
+
+            if epoch == 1:
+                self.generator.save("model_arch-generator_%s_epoch_%d.h5" % (self.trial_name, epoch))
+                print("Saved model to disk")
+                self.discriminator.save("model_arch-discrim_%s_epoch_%d.h5" % (self.trial_name, epoch))
+                print("Saved model to disk")
+                self.combined.save("model_arch-combined_%s_epoch_%d.h5" % (self.trial_name, epoch))
+                print("Saved model to disk")
